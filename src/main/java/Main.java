a54ab106be215cfcff51d20a36cdcf94512cecd4
eignatik <light.ji@yandex.ru>
2016-06-07 22:47:18 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Eugen on 6/7/2016.
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        String fxmlFile = "/fxml/test.fxml";
+        FXMLLoader loader = new FXMLLoader();
+        Parent root = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setTitle("test");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
---
+++ src/main/java/MainController.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Eugen on 6/7/2016.
+ */
+public class MainController {
+}
