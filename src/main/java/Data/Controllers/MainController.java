3e486ec8afe68caab5579cfcb8dfa4ade72bcc9a
eignatik <light.ji@yandex.ru>
2016-06-13 23:47:12 +0300

    Added display todo list and descriptions

--- src/main/java/Data/Controllers/MainController.java
+++ src/main/java/Data/Controllers/MainController.java
@@ -1,9 +1,11 @@
 package Data.Controllers;
 
 import Data.Entity.Project;
-import Data.Log;
+import Data.Entity.ToDo;
 import Data.ModelDataApp;
 import Data.Session;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
@@ -14,11 +16,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.cell.CheckBoxListCell;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.util.Callback;
 
 import java.net.URL;
-import java.util.Observable;
 import java.util.ResourceBundle;
 
 /**
@@ -31,6 +33,11 @@ public class MainController implements Initializable {
     private TextArea projDescr;
     @FXML
     private ListView<Project> projList;
+    @FXML
+    private TextArea todoDescr;
+    @FXML
+    private ListView<ToDo> todoList;
+    private int idProject;
 
 
     @Override
@@ -56,13 +63,63 @@ public class MainController implements Initializable {
         projList.getSelectionModel().selectedItemProperty().addListener((new ChangeListener<Project>() {
             @Override
             public void changed(ObservableValue<? extends Project> observable, Project oldValue, Project newValue) {
-                MainController.this.showDescription(projList.getSelectionModel().getSelectedItem().getDescr());
+                if(newValue==null){return;}
+                idProject = projList.getSelectionModel().getSelectedItem().getIdProj();
+                MainController.this.showProjDescription(projList.getSelectionModel().getSelectedItem().getDescr());
+                MainController.this.showToDo(projList.getSelectionModel().getSelectedItem().getIdProj());
+            }
+        }));
+        todoList.getSelectionModel().selectedItemProperty().addListener((new ChangeListener<ToDo>() {
+            @Override
+            public void changed(ObservableValue<? extends ToDo> observable, ToDo oldValue, ToDo newValue) {
+                if(newValue==null){return;}
+                MainController.this.showTodoDescription(todoList.getSelectionModel().getSelectedItem().getDescr());
             }
         }));
     }
 
-    public void showDescription(String description){
+    public void showProjDescription(String description){
         projDescr.setText(description);
         System.out.println("Selected description: " + description);
+    }
+
+    public void showTodoDescription(String description){
+        todoDescr.setText(description);
+        System.out.println("Selected description: " + description);
+    }
+
+    public void showToDo(int id){
+        if(idProject==0) {return;}
+        ObservableList<ToDo> todoList = FXCollections.observableList(ModelDataApp.getToDoList(idProject));
+        this.todoList.setItems(todoList);
+//        this.todoList.setCellFactory(CheckBoxListCell.forListView(new Callback<ListView<ToDo>, ObservableValue<Boolean>>(){
+//            @Override
+//            public ObservableValue<Boolean> call(ListView<ToDo> p){
+//                BooleanProperty observ = new SimpleBooleanProperty();
+//                observ.addListener(new ChangeListener<Boolean>() {
+//                    @Override
+//                    public void changed(ObservableValue<? extends Boolean> obs, Boolean wasSelected, Boolean isNowSelected) {
+//                        System.out.println("Check box was selected");
+//                    }
+//                });
+//                return observ;
+//            }
+//
+//        }));
+        this.todoList.setCellFactory(new Callback<ListView<ToDo>, ListCell<ToDo>>(){
+            @Override
+            public ListCell<ToDo> call(ListView<ToDo> p){
+                ListCell<ToDo> cell = new ListCell<ToDo>(){
+                    @Override
+                    protected void updateItem(ToDo t, boolean bln){
+                        super.updateItem(t, bln);
+                        if(t != null){
+                            setText(t.isCompleted() + " " + t.getTitle() + "  Employer: " + t.getIdEmp());
+                        }
+                    }
+                };
+                return cell;
+            }
+        });
     }
 }
--- src/main/java/Data/ModelDataApp.java
+++ src/main/java/Data/ModelDataApp.java
@@ -2,6 +2,7 @@ package Data;
 
 import Data.Entity.Employe;
 import Data.Entity.Project;
+import Data.Entity.ToDo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +17,21 @@ public class ModelDataApp {
         List<Project> list = new ArrayList<>();
         String query = "select id_proj, title, descr from project";
         try{
-            list.addAll(DBCon.getConnection().createQuery(query).addColumnMapping("id_proj", "idProj").addColumnMapping("title", "Title").addColumnMapping("descr", "descr").executeAndFetch(Project.class));
+            list.addAll(DBCon.getConnection().createQuery(query).addColumnMapping("id_proj", "idProj").addColumnMapping("title", "title").addColumnMapping("descr", "descr").executeAndFetch(Project.class));
         } catch (Exception e){
             log.error("ModelDataApp ProjectList error - " + e);
         }
         return list;
     }
+    public static List<ToDo> getToDoList(int id){
+        List<ToDo> list = new ArrayList<>();
+        String query = "select id_todo, id_proj, id_emp, title, descr, completed from todo where id_proj=" + id;
+        try{
+            list.addAll(DBCon.getConnection().createQuery(query).addColumnMapping("id_todo", "idToDo").addColumnMapping("id_proj", "idProj").addColumnMapping("id_emp", "idEmp").addColumnMapping("title", "title").addColumnMapping("descr", "descr").executeAndFetch(ToDo.class));
+        } catch (Exception e){
+            log.error("ModelDataApp ToDoList error - " + e);
+        }
+        return list;
+    }
+
 }
